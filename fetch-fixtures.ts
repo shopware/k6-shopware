59e81c7fe0c09766d180fd19070fc5968db1829b
patzick <13100280+patzick@users.noreply.github.com>
2024-02-05 09:32:42 +0100

    feat: update client credentials

--- fetch-fixtures.ts
+++ fetch-fixtures.ts
@@ -7,20 +7,13 @@ import type {
 
 export const adminApiClient = createAdminAPIClient<operations, operationPaths>({
   baseURL: `${process.env.SHOP_URL}/api`,
-});
-
-const sessionData = await adminApiClient.invoke("token post /oauth/token", {
-  client_id: "administration",
-  grant_type: "password",
-  username: process.env.SHOP_ADMIN_USERNAME as string,
-  password: process.env.SHOP_ADMIN_PASSWORD as string,
-  scopes: "write",
-});
-
-adminApiClient.setSessionData({
-  accessToken: sessionData.access_token,
-  refreshToken: sessionData.refresh_token as string,
-  expirationTime: sessionData.expires_in,
+  credentials: {
+    grant_type: "password",
+    client_id: "administration",
+    scopes: "write",
+    username: process.env.SHOP_ADMIN_USERNAME as string,
+    password: process.env.SHOP_ADMIN_PASSWORD as string,
+  },
 });
 
 async function fetchSalesChannel() {
